00da5e2fd0e0aadcadfe35712ab99c9b68c73fab
xenon <xenon@chainkemists.xyz>
2022-05-25 14:30:59 -0400

    feat: initial style-guide config

---
+++ packages/chks-prettier/index.js
@@ -0,0 +1,21 @@
+/**
+ * Some of Prettier's defaults can be overridden by an EditorConfig file. We
+ * define those here to ensure that doesn't happen.
+ *
+ * See: https://github.com/prettier/prettier/blob/main/docs/configuration.md#editorconfig
+ */
+const overridableDefaults = {
+  endOfLine: 'lf',
+  tabWidth: 2,
+  printWidth: 80,
+  useTabs: false,
+}
+
+module.exports = {
+  ...overridableDefaults,
+  singleQuote: true,
+  semi: false,
+  trailingComma: 'all',
+  arrowParens: 'always',
+  bracketSpacing: true,
+}
